1103d09f782f025d995b08972a99b84cdbedaf13
Sarthak  Saini <150471651+Sarthak-Saini265@users.noreply.github.com>
2025-09-29 14:33:28 +0530

    Add expose_headers to CORS configuration

--- api/index.py
+++ api/index.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["POST", "GET", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # --- Your Existing Logic ---
